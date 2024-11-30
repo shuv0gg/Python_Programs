16e0bc7e419263b35b8df63a9885591db5c33b7d
SHUVOgg <mek234778@gmail.comgit config --global user.email you@example.com>
2024-11-30 22:25:19 +0600

    Sum of two number

---
+++ sum.py
@@ -0,0 +1,4 @@
+first_num=int(input("Enter first number :"))
+second_num=int(input("Enter second number :"))
+result=first_num+second_num
+print(f"The summation of {first_num} & {second_num} is {result}")
