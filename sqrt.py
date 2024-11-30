319dcdcbfc8a616103512b4c76b2ea1df9ece156
SHUVOgg <mek234778@gmail.comgit config --global user.email you@example.com>
2024-11-30 23:16:28 +0600

    Square Root

---
+++ sqrt.py
@@ -0,0 +1,10 @@
+import math
+def square_root(x):
+    return x**0.5
+
+first_num=int(input("Enter first number :"))
+second_num=int(input("Enter second number :"))
+a=math.sqrt(first_num)
+print(f"Square root of {first_num} is {a}")
+print(f"Square root of {second_num} is {square_root(second_num)}")
+
